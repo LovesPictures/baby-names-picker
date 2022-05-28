b683104746821a3c38b4be3e7d132707aabe7d70
Jude <lovespictures2@gmail.com>
2022-05-28 12:52:03 +0100

    Updated button filter

--- src/components/FilterButtons.js
+++ src/components/FilterButtons.js
@@ -6,19 +6,34 @@ import React from "react";
 
 /* filtering by  gender 2 */
 const FilterButton = ({ genderFilter, setGenderFilter }) => {
+
+  console.log("genderFilter", genderFilter);
+  const gender = [ "all", "m", "f"]
+ 
   return (
     <div className="filterButtons">
-      <button className="children" onClick={() => setGenderFilter("all")}>
-        <i className="fa solid fa-child"></i>
-      </button>
-      <button className="m" onClick={() => setGenderFilter("m")}>
-        <i className="fa solid fa-child"></i>
-      </button>
-      <button className="f" onClick={() => setGenderFilter("f")}>
-        <i className="fa solid fa-child"></i>
-      </button>
+    {gender.map(element => <button className="children" onClick={() => setGenderFilter (element)}>
+    <i className="fa solid fa-child"></i></button>)}
     </div>
+ 
   );
 };
 
 export default FilterButton;
+
+// const FilterButton = ({ genderFilter, setGenderFilter }) => {
+  
+//   return (
+//     <div className="filterButtons">
+//       <button className="children" onClick={() => setGenderFilter("all")}>
+//         <i className="fa solid fa-child"></i>
+//       </button>
+//       <button className="m" onClick={() => setGenderFilter("m")}>
+//         <i className="fa solid fa-child"></i>
+//       </button>
+//       <button className="f" onClick={() => setGenderFilter("f")}>
+//         <i className="fa solid fa-child"></i>
+//       </button>
+//     </div>
+//   );
+// };
