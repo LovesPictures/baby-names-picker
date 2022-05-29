d7e1fcca99f41edb0f3d52d06611abae4ee7a389
Jude <lovespictures2@gmail.com>
2022-05-29 12:36:15 +0100

    Installed icons

--- src/components/FilterButtons.js
+++ src/components/FilterButtons.js
@@ -1,39 +1,61 @@
 import React from "react";
-//icon examples
-// import { FaHeart, FaRegHeart } from "react-icons/fa";
-// reference
-// https://www.youtube.com/watch?v=w7ejDZ8SWv8&ab_channel=TraversyMedia
 
 /* filtering by  gender 2 */
 const FilterButton = ({ genderFilter, setGenderFilter }) => {
-
-  console.log("genderFilter", genderFilter);
-  const gender = [ "all", "m", "f"]
- 
+  //create an an array of objects to map over
+  const genderButtons = [
+    { gender: "all", iconClass: "fa-solid fa-baby-carriage"},
+    { gender: "m", iconClass:  "fa-solid fa-baby"},
+    { gender: "f", iconClass: "fa-solid fa-baby"},
+  ];
+  // console.log({faChildDress});
   return (
     <div className="filterButtons">
-    {gender.map(element => <button className="children" onClick={() => setGenderFilter (element)}>
-    <i className="fa solid fa-child"></i></button>)}
+      {genderButtons.map((element) => (
+        <button
+          className={element.gender}
+          onClick={() => setGenderFilter(element.gender)}
+        >
+          {/* <FaBaby /> */}
+          <i className={element.iconClass}></i>
+        </button>
+      ))}
     </div>
- 
   );
 };
 
 export default FilterButton;
 
-// const FilterButton = ({ genderFilter, setGenderFilter }) => {
+/* 
+original code 
+using react fonts
+icons from fa = font awesome  examples only uploads icons which are needed
+import { FaBaby, FaBabyCarriage } from "react-icons/fa";
+
+second 
+  //create an object form the array 
+  const genderButtons = [ {gender: "all", iconClass:"fa-solid fa-people"}, 
+                          {gender:"m",iconClass:"fa-solid fa-child"}, 
+                          {gender:"f",iconClass:"fa-solid fa-child-dress"}]
+const FilterButton = ({ genderFilter, setGenderFilter }) => {
   
-//   return (
-//     <div className="filterButtons">
-//       <button className="children" onClick={() => setGenderFilter("all")}>
-//         <i className="fa solid fa-child"></i>
-//       </button>
-//       <button className="m" onClick={() => setGenderFilter("m")}>
-//         <i className="fa solid fa-child"></i>
-//       </button>
-//       <button className="f" onClick={() => setGenderFilter("f")}>
-//         <i className="fa solid fa-child"></i>
-//       </button>
-//     </div>
-//   );
-// };
+first 
+  return (
+    <div className="filterButtons">
+      <button className="children" onClick={() => setGenderFilter("all")}>
+        <i className="fa solid fa-child"></i>
+      </button>
+      <button className="m" onClick={() => setGenderFilter("m")}>
+        <i className="fa solid fa-child"></i>
+      </button>
+      <button className="f" onClick={() => setGenderFilter("f")}>
+        <i className="fa solid fa-child"></i>
+      </button>
+    </div>
+  );
+};
+
+reference
+https://www.youtube.com/watch?v=aor9hlcODUE&ab_channel=EricMurphy
+
+*/
